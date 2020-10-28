db02a347a0ff3f63741e9d40c9e0685f17eeb38b
bethojunior <hgomescostajr@gmail.com>
2020-10-28 20:42:06 -0300

    inital commit

---
+++ assets/js/email.js
@@ -0,0 +1,18 @@
+$("#sendmail").click( function (){
+    swal('Enviado com sucesso','Aguarde nosso retorno','success');
+    let data = {};
+    data.nome = document.getElementById('name').value;
+    data.email = document.getElementById('email').value;
+    data.texto = document.getElementById('message').value;
+
+    $.ajax({
+        url: '/email.php',
+        method: 'POST',
+        data: data,
+        success:function (data) {
+            console.log(data)
+        }
+    })
+
+    swal('Enviado com sucesso','Aguarde nosso retorno','success');
+})
