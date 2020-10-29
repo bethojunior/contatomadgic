eb297830dde83a6af209a4c5209c27c4dd84fdca
bethojunior <hgomescostajr@gmail.com>
2020-10-28 21:21:57 -0300

    link social

--- email.php
+++ email.php
@@ -1,16 +1,17 @@
 
 <?php
 
-$destino = "contato@madgic.com.br";
+$destino1 = "contato@madgic.com.br";
+$destino2 = "betho@madgic.com.br";
+$destino3 = "ranyeli@madgic.com.br";
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $texto = $_POST['texto'];
 $headers = "Enviado por : " . $nome . "\n";
 $mensagem = "Enviado por : " . $nome . "\n"."Contato: ".$email."\n".$texto;
 
-if(mail($destino, $headers , $mensagem)){
-    echo true;
-}else{
-    echo false;
-}
+mail($destino1, $headers , $mensagem);
+mail($destino2, $headers , $mensagem);
+mail($destino3, $headers , $mensagem);
+
 ?>
